e41b683e94e2f9ef0054ed881dd4a448ad46450a
alex <alexadkl@gmail.com>
2024-10-25 09:24:53 +0200

    Mise à jour Guess a number

--- Exercices_individuels/02_Guess_a_number/index.js
+++ Exercices_individuels/02_Guess_a_number/index.js
@@ -1,24 +1,109 @@
+//étape 1
+
 function askNumber(){
-    window.prompt('Entrez un nombre')
+    let givenNumber = prompt('Choose a number')
+    return givenNumber
 }
 
-let givenNumber = askNumber()
+const givenNumber2 = askNumber() 
 
-//const givenNumber = window.prompt('Entrez un nombre')
+//étape 2
 
-let response = givenNumber 
+/* function didIWin(userNumber)
+{
+    if (userNumber === 22)
+        {
+            alert("Bravo ! Vous avez deviné le nombre !")
+        }  
+    else {
+            if (userNumber < 22) 
+                {alert("Plus grand")}
+            else
+                {alert("Plus petit")}
+         }
+}        
 
-function didIWin(response){
-    if 
-        (response == 22)
-        alert("Bravo ! Vous avez deviné le nombre !")    
-    if 
-        (response < 22) 
-        alert("Plus grand")
-    else
-       // (response > 22)
-        alert("Plus petit")
-   
+function gamePlay(){
+    let givenNumber = prompt('Entrez un nombre')
+    didIWin(givenNumber)
+} */
+
+//gamePlay()
+
+// étape 3
+
+function didIwin (givenNumber) {
+    if (givenNumber == 22) {
+        return true;
+    } else {
+        return false;
+    }
+}     
+
+function gamePlay() {
+    let userEntry = askNumber();
+    if (didIwin(userEntry)) {
+        alert ("Yay, you win !");
+    } else {
+        alert("Wrong! Choose another number!");
+        gamePlay(); 
+     }
+    
+}
+gamePlay ()
+
+/* function gamePlay(){
+    
+    let givenNumber = prompt('Entrez un nombre')
+    didIWin(givenNumber)
+    }
+
+    function askNumberToUser () {
+        let givenNumber = prompt("Please enter a number"); 
+        return givenNumber 
+    } */
+    /* function didIWin (givenNumber) {
+        if (givenNumber < 22) {
+            alert("Bigger");
+        } else if (givenNumber > 22) {
+            alert("Smaller");
+        } else if (givenNumber == 22) {
+            alert("Hooray ! You guess it !");
+        }
+    } */
+        
+        
+    
+    
+    
+    
+    
+        
+
+// étape 4
+
+/* function askPlayer1(){
+    let Numberplayer1 = prompt("Choisissez un nombre entre 0 et 50")
+    return Numberplayer1
 }
 
-didIWin(response)
+let numberToGuess = askPlayer1()
+
+function checkNumber(n) {
+    n = Number(n);
+    if (n < 0) 
+    {
+        alert('Type number between 0-50');
+        n = 0;
+    } 
+        else if (n > 50) 
+    {
+        alert('Type number between 0-50');
+        n = 50;
+    } 
+    else {
+        alert('Valid number: ' + n);
+    }
+}
+
+checkNumber(numberToGuess);  */
